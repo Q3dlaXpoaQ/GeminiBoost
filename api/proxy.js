74c0a911eca93cc3ebcba14e28cdf4ed0a07a0ca
Cwz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2025-02-10 23:06:08 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://home.chwz.us.kg/";//your website url
+  let target = "https://generativelanguage.googleapis.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
